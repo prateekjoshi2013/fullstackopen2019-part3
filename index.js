cced9ce590cdf884bed8a9d9adca6b5bf70eebf7
Prateek Joshi <prateekjoshi2013@gmail.com>
2019-09-03 22:11:18 -0400

    changed procfile

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser=require('body-parser')
 const morgan = require('morgan')
+const cors= require('cors')
 
+app.use(cors())
 app.use(bodyParser.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
@@ -87,7 +89,7 @@ app.post('/api/persons',(req,res)=>{
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
